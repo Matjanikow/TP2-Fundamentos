21a02d4fe7de57f6999557ef8a72392c7a54b418
matata <Matjanikow@gmail.com>
2024-10-29 22:14:01 -0300

    Agrego una colision

--- player.gd
+++ player.gd
@@ -3,15 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+var saltos = 3
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		saltos = 3
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and saltos>0:
+		saltos -= 1
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
